6a8db13b7bc16a883a914b17138be87f9ed27837
SISONGXU <37802567+SISONGXU@users.noreply.github.com>
2018-04-16 14:30:30 +0200

    avec HSV

--- hog.cpp
+++ hog.cpp
@@ -17,22 +17,25 @@ cv::HOGDescriptor hog;
 //?? HOGDescriptor hog;
 class ImageConverter
 {
-  ros::NodeHandle nh;
+  ros::NodeHandle nh_;
   image_transport::ImageTransport it_;
   image_transport::Subscriber image_sub_;
+  image_transport::Publisher image_pub_;
   ros::Publisher pub2;
 
   public:
   
   ImageConverter()
-    : it_(nh)
+    : it_(nh_)
   {
-    namedWindow("ciblehumaine", CV_WINDOW_AUTOSIZE);
-	moveWindow( "ciblehumaine",0,600);
-    hog.setSVMDetector(HOGDescriptor::getDefaultPeopleDetector());
-        // Subscrive to input video feed and publish output video feed
-    image_sub_ = it_.subscribe("/bebop/image_raw", 1,&ImageConverter::imageCb, this);
-    pub2 = nh.advertise<geometry_msgs::Point>("/cible_humaine", 1000);
+	  hog.setSVMDetector(HOGDescriptor::getDefaultPeopleDetector());
+	  image_sub_ = it_.subscribe("/bebop/image_raw", 1,&ImageConverter::imageCb, this);
+	  // Subscrive to input video feed and publish output video feed
+	  //?? image_sub_ = it_.subscribe("/bebop/image_raw", 1,&ImageConverter::imageCb, this);
+	  image_pub_ = it_.advertise("/image_converter/output_video", 1);
+	  pub2 = nh_.advertise<geometry_msgs::Point>("/cible_humaine", 1000);
+	  namedWindow("ciblehumaine", CV_WINDOW_AUTOSIZE);
+	  moveWindow( "ciblehumaine",0,600);
   }
 
   
@@ -54,14 +57,33 @@ class ImageConverter
       ROS_ERROR("cv_bridge exception: %s", e.what());
       return;
     }
-
-    cv::Mat img = cv_ptr->image;
 	
-	std::vector<Rect> found, found_filtered;
-	//?? vector<Rect> found, found_filtered;
+	// Perform HSV tranformation
+
+	cv::Mat imgRGB = cv_ptr->image;	
+	
+ 	int iLowH = 0;
+ 	int iHighH = 30;
+
+ 	int iLowS = 120; 
+ 	int iHighS = 235;
+
+ 	int iLowV = 120;
+ 	int iHighV = 255;
+
+	cv::Mat imgHSV;
+	
+	
+	cv::cvtColor(imgRGB, imgHSV, cv::COLOR_BGR2HSV);
+	
+	//cv::Mat frame = cv_ptr->image;
+    //Mat img = cv_ptr->image;
+	
+	vector<Rect> found, found_filtered;
+
 
 	//________LAG________________________________________
-	hog.detectMultiScale(img, found, 0, Size(16,16),Size(0,0), 1.05, 2);
+	hog.detectMultiScale(imgHSV, found, 0, Size(8,8),Size(0,0), 1.05, 2);
 	//?? hog.detectMultiScale(img, found, 0, Size(0,0), Size(16,16), 1.05, 2);
 	//________________________________________________
 	size_t i, j;
@@ -84,15 +106,16 @@ class ImageConverter
 		r.width = cvRound(r.width*0.8);
 		r.y += cvRound(r.height*0.07);
 		r.height = cvRound(r.height*0.8);
-		rectangle(img, r.tl(), r.br(), Scalar(0,255,0), 3);        
+		rectangle(imgRGB, r.tl(), r.br(), Scalar(0,255,0), 3);        
 		geometry_msgs::Point pos_h;
 		pos_h.x = (r.x+(r.width/2));
 		pos_h.y = r.y;
 		pos_h.z= r.height;
 		pub2.publish(pos_h);
 	}
-	imshow("ciblehumaine", img);
-	waitKey(10);
+	image_pub_.publish(cv_ptr->toImageMsg());
+	imshow("ciblehumaine", imgRGB);
+	waitKey(5);
   }
 };
 
